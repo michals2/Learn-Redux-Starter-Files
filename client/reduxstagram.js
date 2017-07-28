7442d749d1d6060a6059017105b9286eb9a11587
Luke <luke.michals@gmail.com>
2017-07-27 19:02:51 -0700

    folder cleanup

---
+++ client/reduxstagram.js
@@ -0,0 +1,9 @@
+// let's go!
+import React from 'react';
+
+import { render } from 'react-dom'
+
+//import css
+import css from './styles/style.styl'
+
+render(<p>hi</p>, document.getElementById('root'))
